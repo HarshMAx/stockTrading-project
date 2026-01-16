6bdef064e68e6761d090686ba4fd9b20567056fb
harshMAx <harshsolanki4121@gmail.com>
2026-01-16 12:44:54 +0530

    create a pricing page

--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from './Hero';
+import OpenAccount from '../../OpenAccount'
 import Brockrage from './Brockrage';
 
 function PricingPage() {
@@ -7,6 +8,7 @@ function PricingPage() {
         // <h1>PrincingPage</h1>
         <>
             <Hero />
+            <OpenAccount />
             <Brockrage />
         </>
      );
